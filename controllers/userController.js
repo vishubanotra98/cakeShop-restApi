94f48387b52556ea162ce8c4c7b8b944bd911512
Vishu Banotra <111637458+VishuBanotra@users.noreply.github.com>
2023-09-15 11:52:46 +0530

    login cookies

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,35 +25,6 @@ export const registerUser = asyncError(async (req, res, next) => {
   }
 });
 
-// Login user
-// export const loginUser = asyncError(async (req, res, next) => {
-//   const { username, password } = req.body;
-
-//   try {
-//     const user = await User.findOne({ username });
-
-//     if (!user) {
-//       return res.status(401).json({ message: "Authentication failed" });
-//     }
-
-//     if (user.password !== password) {
-//       return res.status(401).json({ message: "Incorrect Password" });
-//     }
-
-//     const token = jwt.sign(
-//       { username: user.username, id: user._id, role: user.role },
-//       process.env.JWT_SECRET
-//     );
-//     // res.json({message: "User Logged In successfully", token });
-//     res.cookie("token", token, {
-//       sameSite: "none",
-//       secure: false,
-//     });
-//     res.json({ message: "Login Successfully" });
-//   } catch (error) {
-//     next(error);
-//   }
-// });
 
 export const loginUser = asyncError(async (req, res, next) => {
   const { username, password } = req.body;
@@ -74,7 +45,7 @@ export const loginUser = asyncError(async (req, res, next) => {
       );
 
       res.cookie("token", token, {
-        secure: true,
+        secure: false,
         sameSite: "none",
       });
       res.json({ message: "User Logged In" });
